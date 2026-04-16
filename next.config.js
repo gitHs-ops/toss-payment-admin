afe5f6362438c917cdb9076a871338e7ead1b6b5
kimhs1234 <khsq2011@gmail.com>
2026-04-16 19:52:01 +0900

    fix: remove invalid next.config allowedDevHosts

--- next.config.js
+++ next.config.js
@@ -1,7 +1,4 @@
 ﻿/** @type {import('next').NextConfig} */
-const nextConfig = {
-  // ngrok 등 외부 터널링 도구로 접속 허용
-  allowedDevHosts: [".ngrok-free.app", ".ngrok-free.dev", ".ngrok.io"],
-};
+const nextConfig = {};
 
 module.exports = nextConfig;
